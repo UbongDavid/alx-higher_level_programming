a2150f5901366db0e8427cbb3a90dbe44843b8b0
ubongdu <ubongdumoette@gmail.com>
2024-03-20 16:12:22 +0100

    Correct Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
-from add_0 import add
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 
-
-def main():
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-if __name__ == "__main__":
-    main()
