74dc65499a0405515de1a33678bb048e1a302187
ubongdu <ubongdumoette@gmail.com>
2024-03-20 16:09:51 +0100

    Correct Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
 
-from sys import argv
+    num_of_args = len(argv)
+    sum = 0
 
-num_of_args = len(argv)
-sum = 0
-
-for number in range(1, num_of_args):
-    sum += int(argv[number])
-print(sum)
+    for number in range(1, num_of_args):
+        sum += int(argv[number])
+    print(sum)
